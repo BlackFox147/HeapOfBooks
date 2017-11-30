7074e05b48afeb7b62cde8e931d8534f9629d3c7
Юлия Момотова 550501 <black_fox147@mail.ru>
2017-11-30 21:43:30 +0300

    Создание проекта. Подключение к базе данных.

---
+++ Code/Code/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Code.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Code
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // add data 
+            using (UserContext db = new UserContext())
+            {
+                // создаем объекты Book 
+                Collection collection1 = new Collection { Name = "Граф Монтекристо", DateСreation = DateTime.Now };
+                Collection collection2 = new Collection { Name = "Властелин колец", DateСreation = DateTime.Now };
+                Collection collection3 = new Collection { Name = "Три толсяка", DateСreation = DateTime.Now };
+
+                // добавляем в базу данных 
+                db.Collections.Add(collection1);
+                db.Collections.Add(collection2);
+                db.Collections.Add(collection3);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Code/Code/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Code.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+               : base("HeapOfBooksContext")
+        { }
+
+        public DbSet<Collection> Collections { get; set; }
+    }
+}
